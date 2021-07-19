72197e8138649cb4a0db4772e9a82a467cce0bb7
phwoo0901 <phwoo1004@gmail.com>
2021-07-19 23:20:46 +0900

    Nodejs-MySQL study day9 'author' 목록을 추가하여 author TALBLE을 보여준다.
    create(name값, profile값을 입력)
    update(name값, profile값 수정 가능)
    delete(id값에 따라 author TABLE의 해당 행을 삭제 + author_id값을 이용한 목록 삭제 기능)

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var db = require('./lib/db'); // 정리정돈 - db접속
 var topic = require('./lib/topic'); // 정리정돈 - SQL
+var author = require('./lib/author');
 
 var app = http.createServer(function(request, response) {
   var _url = request.url;
@@ -20,13 +21,13 @@ var app = http.createServer(function(request, response) {
       topic.page(request, response);
     }
   }
-  else if (pathname === '/create') { // WEB, CSS, HTML, JavaScript, ... 등에서 'create'를 누르면 나오는 웹페이지
+  else if (pathname === '/create') { // WEB, CSS, HTML, JavaScript, ... 등에서 'create'버튼을 클릭하면 나오는 웹페이지
     topic.create(request, response);
   }
   else if (pathname === '/create_process') { // create 웹페이지에서 '제출'을 클릭 (생성된 웹페이지를 표시)
     topic.create_process(request, response);
   }
-  else if (pathname === '/update') { // WEB, CSS, HTML, JavaScript, ... 등에서 'update'를 누르면 나오는 웹페이지
+  else if (pathname === '/update') { // WEB, CSS, HTML, JavaScript, ... 등에서 'update'버튼을 클릭하면 나오는 웹페이지
     topic.update(request, response);
   }
   else if (pathname === '/update_process') { // update 웹페이지에서 '제출'을 클릭 (수정된 웹페이지를 표시)
@@ -35,6 +36,23 @@ var app = http.createServer(function(request, response) {
   else if (pathname === '/delete_process') { // delete 웹페이지에서 '제출'을 클릭 (삭제가 완료되면 home을 표시)
     topic.delete_process(request, response);
   }
+
+  else if (pathname === '/author') {
+    author.home(request, response);
+  }
+  else if (pathname === '/author/create_process') {
+    author.create_process(request, response);
+  }
+  else if (pathname === '/author/update') {
+    author.update(request, response);
+  }
+  else if (pathname === '/author/update_process') {
+    author.update_process(request, response);
+  }
+  else if (pathname === '/author/delete_process') {
+    author.delete_process(request, response);
+  }
+
   else { // 404 Not Found
     response.writeHead(404);
     response.end('Not Found');
