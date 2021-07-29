364affab9a3a4a4e7eff90ced43dea57606fe1ff
phwoo0901 <phwoo1004@gmail.com>
2021-07-29 22:58:58 +0900

    Cookie & Auth study day2 Secure (https로 접근해야 함) vs HttpOnly (JS로 접근하지 못하도록 함)

--- file/cookie.js
+++ file/cookie.js
@@ -11,7 +11,9 @@ http.createServer(function(request, response) {
     'Set-Cookie':[
       'yummy_cookie=choco',
       'tasty_cookie=strawberry',
-      `Permanent=cookies; Max-Age=${60*60*24*30}`
+      `Permanent=cookies; Max-Age=${60*60*24*30}`,
+      'Secure=Secure; Secure',
+      'HttpOnly=HttpOnly; HttpOnly'
     ]
   });
   response.end('Cookie!!');
