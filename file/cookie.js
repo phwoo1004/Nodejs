5b419e0165729824f993eccc2070f6793130d6cc
phwoo0901 <phwoo1004@gmail.com>
2021-07-29 22:53:13 +0900

    Cookie & Auth study day2 Session vs Permanent

--- file/cookie.js
+++ file/cookie.js
@@ -4,11 +4,15 @@ http.createServer(function(request, response) {
   console.log(request.headers.cookie);
   var cookies = {};
   if (request.headers.cookie !== undefined) {
-    var cookies = cookie.parse(request.headers.cookie);
+    cookies = cookie.parse(request.headers.cookie);
   }
   console.log(cookies.yummy_cookie);
   response.writeHead(200, {
-    'Set-Cookie':['yummy_cookie=choco', 'tasty_cookie=strawberry']
+    'Set-Cookie':[
+      'yummy_cookie=choco',
+      'tasty_cookie=strawberry',
+      `Permanent=cookies; Max-Age=${60*60*24*30}`
+    ]
   });
   response.end('Cookie!!');
 }).listen(3000);
