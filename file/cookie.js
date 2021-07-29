318fb6276e5041ca59086e5dfef12ec5a9477d22
phwoo0901 <phwoo1004@gmail.com>
2021-07-29 22:43:52 +0900

    Cookie & Auth study day2 Read Cookie

--- file/cookie.js
+++ file/cookie.js
@@ -1,5 +1,12 @@
 var http = require('http');
+var cookie = require('cookie');
 http.createServer(function(request, response) {
+  console.log(request.headers.cookie);
+  var cookies = {};
+  if (request.headers.cookie !== undefined) {
+    var cookies = cookie.parse(request.headers.cookie);
+  }
+  console.log(cookies.yummy_cookie);
   response.writeHead(200, {
     'Set-Cookie':['yummy_cookie=choco', 'tasty_cookie=strawberry']
   });
