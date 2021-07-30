d9f08b7739d5fb1424198a5ab7e4d7bffcdb01d7
phwoo0901 <phwoo1004@gmail.com>
2021-07-30 23:42:58 +0900

    Cookie & Auth study day3 Create 'Login UI'

--- main.js
+++ main.js
@@ -53,6 +53,48 @@ var app = http.createServer(function(request, response) {
     author.delete_process(request, response);
   }
 
+  else if (pathname === '/login') {
+    fs.readdir('./data', function(error, filelist) {
+      var title = 'Login';
+      var list = template.List(filelist);
+      var html = template.HTML(title, list,
+        `
+        <form action="login_process" method="post">
+          <p><input type="text" name="email" placeholder="email"></p>
+          <p><input type="password" name="password" placeholder="password"></p>
+          <p><input type="submit"></p>
+        </form>
+        `,
+        `<a href="/create">create</a>`
+      );
+      response.writeHead(200);
+      response.end(html);
+    });
+  }
+  else if (pathname === '/login_process') {
+    var body = '';
+    request.on('data', function(data) {
+      body += data;
+    });
+    request.on('end', function() {
+      var post = qs.parse(body);
+      if (post.email === 'nodejs' && post.password === '111111') {
+        response.writeHead(302, {
+          'Set-Cookie':[
+            `email=${post.email}`,
+            `password=${post.password}`,
+            'nickname=nodejs'
+          ],
+          Location: '/'
+        });
+        response.end();
+      }
+      else {
+        response.end('Who?');
+      }
+    });
+  }
+
   else { // 404 Not Found
     response.writeHead(404);
     response.end('Not Found');
