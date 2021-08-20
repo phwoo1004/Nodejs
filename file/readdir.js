984c250c969ed75181c3492e869c77df55268d59
phwoo0901 <phwoo1004@gmail.com>
2021-08-20 22:08:46 +0900

    Message 1

--- file/readdir.js
+++
@@ -1,6 +0,0 @@
-var testFolder = './data'; // ./ : 현재 directory
-var fs = require('fs');
-
-fs.readdir(testFolder, function(error, filelist) {
-  console.log(filelist);
-})
--- password.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  id : 'phwoo1004',
-  password : '17011696'
-}
