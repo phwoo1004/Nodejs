b8abf7353d9d402661b0a45141973c7e4015cb0c
phwoo0901 <phwoo1004@gmail.com>
2021-08-26 17:02:34 +0900

    Reminding : FileSystem ~ Exception (Chapter 3)

---
+++ exception/error3.js
@@ -0,0 +1,10 @@
+/*
+프로미스의 에러는 catch하지 않아도 알아서 처리
+하지만 프로미스의 에러를 알아서 처리하는 동작은 노드 버전이 올라감에 따라 바뀔 수 있음
+따라서 프로미스를 사용할 때는 항상 catch를 붙여주는 것을 권장
+*/
+const fs = require('fs').promises;
+
+setInterval(() => {
+    fs.unlink('./abcdefg.js');
+}, 1000);
