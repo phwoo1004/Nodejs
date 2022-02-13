e50117f43d9cf16d563167aa6e882143d0e9e057
phwoo0901 <phwoo1004@gmail.com>
2022-02-13 17:39:57 +0900

    Express study - multer, router

--- express/app.js
+++ express/app.js
@@ -6,8 +6,13 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config();
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user');
 const app = express();
 app.set('port', process.env.PORT || 3000);
+// pug
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(morgan('dev')); // combined, common, short, tiny, ...
 app.use('/', express.static(path.join(__dirname, 'public')));
@@ -24,6 +29,47 @@ app.use(session({
     },
     name: 'session-cookie',
 }));
+
+// router
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send('Not Found');
+});
+
+// multer
+const multer = require('multer');
+const fs = require('fs');
+try {
+    fs.readdirSync('uploads');
+} catch (error) {
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+    fs.mkdirSync('uploads');
+}
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done) {
+            done(null, 'uploads/');
+        }, 
+        filename(req, file, done) {
+            const ext = path.extname(file.originalname);
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        }, 
+    }), 
+    limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+app.get('/upload', (req, res) => {
+    res.sendFile(path.join(__dirname, 'multipart.html'));
+});
+app.post('/upload', 
+    upload.fields([{ name: 'image1' }, { name: 'image2' }]), 
+    (req, res) => {
+        console.log(req.files, req.body);
+        res.send('ok');
+    },
+);
 
 app.use((req, res, next) => {
     console.log('모든 요청에 다 실행됩니다.');
