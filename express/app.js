2098734c41ca96094d70848a91421810e27ae9cb
phwoo0901 <phwoo1004@gmail.com>
2022-02-27 20:16:24 +0900

    Express study - nunjucks

--- express/app.js
+++ express/app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const dotenv = require('dotenv');
 const path = require('path');
+const nunjucks = require('nunjucks');
 
 dotenv.config();
 const indexRouter = require('./routes');
@@ -12,12 +13,19 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 // pug
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'html');
+
+// nunjucks
+nunjucks.configure('views', {
+    express: app, 
+    watch: true,
+});
 
 app.use(morgan('dev')); // combined, common, short, tiny, ...
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+/*
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
     resave: false,
@@ -29,18 +37,22 @@ app.use(session({
     },
     name: 'session-cookie',
 }));
+*/
 
 // router
 app.use('/', indexRouter);
 app.use('/user', userRouter);
 
 app.use((req, res, next) => {
-    res.status(404).send('Not Found');
+    const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
 });
 
 // multer
 const multer = require('multer');
 const fs = require('fs');
+
 try {
     fs.readdirSync('uploads');
 } catch (error) {
@@ -59,7 +71,6 @@ const upload = multer({
     }), 
     limits: { fileSize: 5 * 1024 * 1024 },
 });
-
 app.get('/upload', (req, res) => {
     res.sendFile(path.join(__dirname, 'multipart.html'));
 });
@@ -83,9 +94,11 @@ app.get('/', (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send(err.message);
-});
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
